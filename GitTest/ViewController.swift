ed513de5eb2072bbe5c9d1dfda7dc0fb6909de80
Andy Warden <awarden@andy-wardens-macbook-pro.local>
2017-08-19 10:07:03 +0100

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Andy Warden on 19/08/2017.
+//  Copyright © 2017 Andy Warden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
